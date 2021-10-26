b2ac14fc79773bbff4b500de2ccdb9da049f8d7f
Simone Rossi Tisbeni <simorossi92@outlook.it>
2021-10-26 17:56:35 +0200

    Fix to use Vector2f for position

--- src/ant.cpp
+++ src/ant.cpp
@@ -1,12 +1,12 @@
 #include <ants/ant.hpp>
 
-Ant::Ant(int posX, int posY) : m_posX(posX), m_posY(posY) {}
+Ant::Ant(sf::Vector2f pos) : m_pos(pos) {}
 
 sf::CircleShape Ant::makeShape() {
   float radius = 3;
   auto antShape = sf::CircleShape(radius);
   antShape.setOrigin({radius, radius});
   antShape.setScale({3, 1});
-  antShape.setPosition({m_posX, m_posY});
+  antShape.setPosition(m_pos);
   return antShape;
 }
