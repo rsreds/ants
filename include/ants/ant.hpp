7189298751ee4e5d0c7b47a075566424ae0996cd
Simone Rossi Tisbeni <rsreds@users.noreply.github.com>
2021-11-03 09:35:28 +0100

    Feature/ant class (#10)
    * Add Ant direction
    
    * Add mouse following routine
    
    * Improve ant shape
    
    * Improve ant shape
    
    * Add ant movement

--- include/ants/ant.hpp
+++ include/ants/ant.hpp
@@ -7,9 +7,16 @@ namespace ants {
 class Ant : public sf::Drawable, public sf::Transformable {
  private:
   sf::CircleShape m_shape;
+  float m_speed = 100;
+  sf::Vector2f m_direction;
 
  public:
   Ant(sf::Vector2f pos);
+  void move(float const& elapsedTime);
+  sf::Vector2f getDirection() const;
+  float setSpeed() const;
+  void setDirection(sf::Vector2f const& dir);
+  void setSpeed(float const& spd);
   virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
 };
 }  // namespace ants
--- src/ant.cpp
+++ src/ant.cpp
@@ -1,18 +1,42 @@
+#include <vector2/vector2.h>
+
 #include <ants/ant.hpp>
 
 namespace ants {
-Ant::Ant(sf::Vector2f pos) {
-  float radius = 3;
-  auto antShape = sf::CircleShape(radius);
-  antShape.setOrigin({radius, radius});
-  antShape.setScale({3, 1});
-  m_shape = antShape;
-  setPosition(pos);
-}
+Ant::Ant(sf::Vector2f pos) { setPosition(pos); }
 
 void Ant::draw(sf::RenderTarget& target, sf::RenderStates states) const {
   states.transform *= getTransform();
-  target.draw(m_shape, states);
+  float radius = 3;
+  auto abdomenShape = sf::CircleShape(radius);
+  abdomenShape.setOrigin({radius, radius});
+  abdomenShape.setPosition({-1.5f * radius, 0.f});
+
+  auto thoraxShape = sf::CircleShape(radius * 0.7f);
+  thoraxShape.setOrigin({radius * 0.7f, radius * 0.7f});
+  thoraxShape.setScale({3, 0.7});
+
+  auto headShape = sf::CircleShape(radius * 0.8f);
+  headShape.setOrigin({radius * 0.8f, radius * 0.8f});
+  headShape.setPosition({1.5f * radius, 0.f});
+
+  target.draw(abdomenShape, states);
+  target.draw(thoraxShape, states);
+  target.draw(headShape, states);
+}
+
+sf::Vector2f Ant::getDirection() const { return m_direction; }
+float Ant::setSpeed() const { return m_speed; }
+void Ant::setDirection(sf::Vector2f const& dir) {
+  auto newAngle = sf::rad2deg(std::atan2(m_direction.y, m_direction.x));
+  setRotation(newAngle);
+  m_direction = sf::normalizeCopy(dir);
+}
+void Ant::setSpeed(float const& spd) { m_speed = spd; }
+void Ant::move(float const& elapsedTime) {
+  sf::Vector2f velocity{m_direction};
+  sf::setMagnitude(velocity, m_speed);
+  setPosition(getPosition() + velocity * elapsedTime);
 }
 
 }  // namespace ants
