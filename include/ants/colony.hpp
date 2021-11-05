1b08fd88bfb406e83dfd7ec40e34b7513f154112
Simone Rossi Tisbeni <simorossi92@outlook.it>
2021-11-05 11:30:57 +0100

    Add spawning logic

--- include/ants/colony.hpp
+++ include/ants/colony.hpp
@@ -9,17 +9,21 @@ class Colony {
  private:
   Anthill m_anthill;
   size_t m_maxPopulation;
-  std::vector<Ant> m_ants;
 
  public:
+  std::vector<Ant> m_ants;
   Colony(sf::Vector2f pos, size_t maxPopulation)
-      : m_anthill(pos), m_maxPopulation(maxPopulation) {}
+      : m_anthill(pos), m_maxPopulation(maxPopulation) {
+    m_ants.reserve(m_maxPopulation);
+  }
   Anthill getAnthill() const { return m_anthill; };
   void spawn() {
     if (m_ants.size() < m_maxPopulation) {
-      m_ants.emplace_back(m_anthill.getPosition();)
+      m_ants.emplace_back(m_anthill.getPosition());
     }
   }
+  size_t getPopulation() const { return m_ants.size(); }
+  size_t getMaxPopulation() const { return m_maxPopulation; }
 };
 
 }  // namespace ants
