82212b0f05df3a0659241dd29f454b7fe9b02b6d
Simone Rossi Tisbeni <simorossi92@outlook.it>
2021-11-05 09:24:24 +0100

    Add direction default initialization
    To avoid empty initialization when mouse is out of bounds

--- include/ants/ant.hpp
+++ include/ants/ant.hpp
@@ -9,7 +9,7 @@ class Ant : public sf::Drawable, public sf::Transformable {
  private:
   sf::CircleShape m_shape;
   float m_speed = 100;
-  sf::Vector2f m_direction;
+  sf::Vector2f m_direction{0.f, 0.f};
   sf::Color m_color;
 
  public:
