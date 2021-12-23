33c790f44217533bd08507f9af70512fa9e3f9d1
Jacopo Gasparetto <jacopo.gasparetto@ammagamma.com>
2021-12-23 18:03:54 +0100

    Refactor bounds check + 200 ants

--- include/ants/world.hpp
+++ include/ants/world.hpp
@@ -14,9 +14,10 @@ class World {
   std::vector<Colony> m_colonies;
   std::vector<FoodSource> m_foodSources;
   std::vector<Marker> m_markers;
+  sf::Vector2i m_woldSize;
 
  public:
-  World() {
+  explicit World(sf::Vector2i const & worldSize) : m_woldSize{worldSize} {
     m_colonies.emplace_back(sf::Vector2f{150.f, 300.f}, 100);
     m_foodSources.emplace_back(100, sf::Vector2f{200.f, 350.f});
   }
@@ -61,6 +62,33 @@ inline void World::updateAnt(Colony& colony, Ant& ant) {
   }
   auto strongestMarker =
       findStrongestAdjacent(ant.getPosition(), targetMarker, m_markers);
+
+  auto x = ant.getPosition().x;
+  auto y = ant.getPosition().y;
+
+  float turnFactor = 0.05; // With value less than 1 the ant tunnels the walls
+  float margin = 25.f;
+  auto currentDirection = ant.getDirection();
+
+  if (x <= margin) {
+    currentDirection.x += turnFactor;
+    ant.setDirection(currentDirection);
+    return;
+  } else if (x >= static_cast<float>(m_woldSize.x) - margin) {
+    currentDirection.x -= turnFactor;
+    ant.setDirection(currentDirection);
+    return;
+  }
+  if (y <= margin) {
+    currentDirection.y += turnFactor;
+    ant.setDirection(currentDirection);
+    return;
+  } else if (y >= static_cast<float>(m_woldSize.y) - margin) {
+    currentDirection.y -= turnFactor;
+    ant.setDirection(currentDirection);
+    return;
+  }
+
   if (strongestMarker == m_markers.end())
     ant.setDirection(ant.getDirection() + randomDirection());
   else
--- src/ant.cpp
+++ src/ant.cpp
@@ -48,23 +48,6 @@ void Ant::setSpeed(float const& spd) { m_speed = spd; }
 
 void Ant::updatePosition(float const& elapsedTime, sf::Vector2u const& bounds) {
   sf::Vector2f velocity{m_direction};
-
-  auto x = getPosition().x;
-  auto y = getPosition().y;
-
-  float turnFactor = 0.75; // With value less than 1 the ant tunnels the walls
-  float margin = 10.f;
-
-  if (x <= margin)
-    velocity.x += turnFactor;
-  else if (x > static_cast<float>(bounds.x) - margin)
-    velocity.x -= turnFactor;
-
-  if (y <= margin)
-    velocity.y += turnFactor;
-  else if (y >= static_cast<float>(bounds.y) - margin)
-    velocity.y -= turnFactor;
-
   sf::setMagnitude(velocity, m_speed);
 //  setPosition(getPosition() + velocity * elapsedTime);
   move(velocity * elapsedTime);
