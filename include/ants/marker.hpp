914f68cadffbd1520c48ba1c02e1717c0809b36f
Jacopo Gasparetto <jacopo.gasparetto@gmail.com>
2021-12-24 16:17:40 +0100

    Refactor and documentation

--- include/ants/marker.hpp
+++ include/ants/marker.hpp
@@ -12,7 +12,7 @@
 namespace ants {
 
 // Keep SIZE as last element to compute how big the enum is
-enum MarkerType { toBase, toFood, SIZE };
+enum MarkerType { toBase, toFood };
 
 class Marker : public sf::Transformable {
  private:
@@ -56,31 +56,35 @@ inline std::vector<Marker>::iterator findStrongestAdjacent(
                           [](Marker& a, Marker& b) {
                             return a.getRemainingLife() < b.getRemainingLife();
                           });
-};
+}
 
 // Rows x Columns
 template <size_t COLS, size_t ROWS>
 using Map_t = std::array<std::array<int, COLS>, ROWS>;
-template <size_t Size>
-using AllMarkers_t = std::array<std::array<int, Size>, MarkerType::SIZE>;
-struct HeatMapIndex {
-  int row;
-  int col;
+
+struct HeatmapIndex {
+  size_t col;
+  size_t row;
 };
 
+/**
+ * Static 2D Container (a.k.a 2D fixed array)
+ * @tparam COLS Number of columns
+ * @tparam ROWS Number of rows
+ */
 template <size_t COLS, size_t ROWS>
 class Heatmap {
  private:
   Map_t<COLS, ROWS> m_map{};  // rows x cols
   sf::Vector2i m_windowSize;
-  float m_dw;  // Box's width
-  float m_dh;  // Box's height
 
  public:
+  /**
+   * Initialize Heatmap
+   * @param windowSize Screen windows size to compute columns and rows sizes
+   */
   explicit Heatmap(sf::Vector2i const& windowSize)
-      : m_windowSize{windowSize},
-        m_dw{static_cast<float>(windowSize.x) / COLS},
-        m_dh{static_cast<float>(windowSize.y) / ROWS} {}
+      : m_windowSize{windowSize} {}
 
   void printHeatMap() const {
     for (auto row : m_map) {
@@ -92,30 +96,45 @@ class Heatmap {
     std::cout << '\n';
   }
 
-  [[nodiscard]] inline sf::Vector2f getCellSize() const { return {m_dw, m_dh}; }
-
-  inline HeatMapIndex getIndexFromPosition(sf::Vector2f const& position) {
+  /**
+   * Get the corresponding 2D index from screen position in pixels
+   * @param position World position in pixel
+   * @return HeatmapIndex
+   */
+  inline HeatmapIndex getIndexFromPosition(sf::Vector2f const& position) {
     assert(position.x >= 0 && position.x <= m_windowSize.x &&
            position.y >= 0 && position.y <= m_windowSize.y);
-    int col = std::floor(position.x / m_windowSize.x * COLS);
-    int row = std::floor(position.y / m_windowSize.y * ROWS);
-    return {row, col};
+    size_t col = std::floor(position.x / m_windowSize.x * COLS);
+    size_t row = std::floor(position.y / m_windowSize.y * ROWS);
+    return {col, row};
   }
 
+  /**
+   * Increment by one the value at the corresponding element in world coordinates
+   * @param position World position in pixel
+   */
   inline void incrementByOneAt(sf::Vector2f const& position) {
     auto index = getIndexFromPosition(position);
     m_map.at(index.row).at(index.col) += 1;
   }
 
+  /**
+   * Decrement by one the value at the corresponding element in world coordinates
+   * @param position World position in pixel
+   */
   inline void decrementByOneAt(sf::Vector2f position) {
     auto index = getIndexFromPosition(position);
     if (m_map.at(index.row).at(index.col) > 0)
       m_map.at(index.row).at(index.col) -= 1;
   }
 
-  [[nodiscard]] int getValueAtIndex(int col, int row) const {
-    assert(col >= 0 && col < COLS && row >= 0 && row < COLS);
-    return m_map.at(row).at(col);
+  /**
+   * Ge element's value at HeatmapIndex
+   * @param position World position in pixel
+   */
+  [[nodiscard]] int getValueAtIndex(HeatmapIndex index) const {
+    assert(index.col >= 0 && index.col < COLS && index.row >= 0 && index.row < COLS);
+    return m_map.at(index.row).at(index.col);
   }
 };
 }  // namespace ants
--- test/test_heatmap.cpp
+++ test/test_heatmap.cpp
@@ -8,25 +8,25 @@ TEST_CASE("Operations") {
   ants::Heatmap<6, 4> heatMap{{60, 40}};
   heatMap.incrementByOneAt({22, 35});
   heatMap.printHeatMap();
-  CHECK_EQ(heatMap.getValueAtIndex(2, 3), 1);
+  CHECK_EQ(heatMap.getValueAtIndex({2, 3}), 1);
 
   heatMap.incrementByOneAt({22, 35});
   heatMap.printHeatMap();
-  CHECK_EQ(heatMap.getValueAtIndex(2, 3), 2);
+  CHECK_EQ(heatMap.getValueAtIndex({2, 3}), 2);
 
   heatMap.incrementByOneAt({33, 12});
   heatMap.printHeatMap();
-  CHECK_EQ(heatMap.getValueAtIndex(3, 1), 1);
+  CHECK_EQ(heatMap.getValueAtIndex({3, 1}), 1);
 
   ants::Heatmap<80, 60> heatmap2 {{800, 600}};
   heatmap2.incrementByOneAt({22, 45});
-  CHECK_EQ(heatmap2.getValueAtIndex(2, 4), 1);
+  CHECK_EQ(heatmap2.getValueAtIndex({2, 4}), 1);
 
   heatmap2.incrementByOneAt({22, 45});
-  CHECK_EQ(heatmap2.getValueAtIndex(2, 4), 2);
+  CHECK_EQ(heatmap2.getValueAtIndex({2, 4}), 2);
 
   heatmap2.incrementByOneAt({33, 12});
-  CHECK_EQ(heatmap2.getValueAtIndex(3, 1), 1);
+  CHECK_EQ(heatmap2.getValueAtIndex({3, 1}), 1);
 
   heatmap2.incrementByOneAt({50, 50});
   heatmap2.incrementByOneAt({50, 50});
@@ -34,18 +34,18 @@ TEST_CASE("Operations") {
   heatmap2.incrementByOneAt({49.99, 49.99});
   heatmap2.incrementByOneAt({50, 50});
   heatmap2.decrementByOneAt({50, 50});
-  CHECK_EQ(heatmap2.getValueAtIndex(5, 5), 3);
+  CHECK_EQ(heatmap2.getValueAtIndex({5, 5}), 3);
 
   ants::Heatmap<10, 10> heatmap3 {{800, 600}};
   heatmap3.incrementByOneAt({222, 450});
-  CHECK_EQ(heatmap3.getValueAtIndex(2, 7), 1);
+  CHECK_EQ(heatmap3.getValueAtIndex({2, 7}), 1);
   heatmap3.printHeatMap();
 
   heatmap3.incrementByOneAt({222, 450});
-  CHECK_EQ(heatmap3.getValueAtIndex(2, 7), 2);
+  CHECK_EQ(heatmap3.getValueAtIndex({2, 7}), 2);
 
   heatmap3.incrementByOneAt({333, 120});
-  CHECK_EQ(heatmap3.getValueAtIndex(4, 2), 1);
+  CHECK_EQ(heatmap3.getValueAtIndex({4, 2}), 1);
 
   heatmap3.incrementByOneAt({500, 500});
   heatmap3.incrementByOneAt({500, 500});
@@ -53,12 +53,12 @@ TEST_CASE("Operations") {
   heatmap3.incrementByOneAt({450, 499});
   heatmap3.incrementByOneAt({500, 500});
   heatmap3.decrementByOneAt({500, 500});
-  CHECK_EQ(heatmap3.getValueAtIndex(6, 8), 3);
+  CHECK_EQ(heatmap3.getValueAtIndex({6, 8}), 3);
 
 
   ants::Heatmap<32, 24> heatmap4 {{800, 600}};
   heatmap4.incrementByOneAt({798, 598});
   heatmap4.printHeatMap();
-  CHECK_EQ(heatmap4.getValueAtIndex(31, 23), 1);
+  CHECK_EQ(heatmap4.getValueAtIndex({31, 23}), 1);
 
 }
